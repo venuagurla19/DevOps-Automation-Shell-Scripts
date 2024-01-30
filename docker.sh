d1f4b3690ac8bd894e057c5c863403f697c8660d
venu <agurlavenu1@gmail.com>
2024-01-30 11:15:24 +0530

    commit

---
+++ docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Update the package manager and install Docker
+sudo apt-get update -y
+sudo apt-get install -y docker.io
+
+# Start the Docker service
+sudo systemctl start docker
+
+# Enable Docker to start on boot
+sudo systemctl enable docker
+
+# Pull and run a simple Nginx web server container
+sudo docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
+sudo docker run -d --name jenkins -p 8080:8080 jenkins/jenkins:latest
---
+++ git.sh
@@ -0,0 +1,7 @@
+echo "# Shell-Scripting" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/venuagurla19/Shell-Scripting.git
+git push -u origin main
