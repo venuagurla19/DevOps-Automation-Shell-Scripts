6d4bc3e36c445b6718519c69b8b71966e76c2964
Agurla venu <142922823+venuagurla19@users.noreply.github.com>
2024-02-11 16:09:33 +0530

    Update Jfrog.sh

--- Jfrog.sh
+++ Jfrog.sh
@@ -1,6 +1,6 @@
 # To determine your distribution, run lsb_release -c or cat /etc/os-release
 # Example:echo "deb https://releases.jfrog.io/artifactory/artifactory-pro-debs xenial main" | sudo tee -a /etc/apt/sources.list;
 apt-get install openjdk-11-jre -y
-wget -qO - https://releases.jfrog.io/artifactory/api/gpg/key/public | sudo apt-key add -;
-echo "deb https://releases.jfrog.io/artifactory/artifactory-pro-debs {distribution} main" | sudo tee -a /etc/apt/sources.list;
+wget -qO - https://releases.jfrog.io/artifactory/api/gpg/key/public | sudo apt-key add -
+echo "deb https://releases.jfrog.io/artifactory/artifactory-pro-debs focal main" | sudo tee -a /etc/apt/sources.list
 sudo apt-get update && sudo apt-get install jfrog-artifactory-pro
