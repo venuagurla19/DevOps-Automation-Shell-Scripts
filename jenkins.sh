9df4880241de5add5bb51edc6c836460b6f2272c
Agurla venu <142922823+venuagurla19@users.noreply.github.com>
2024-02-22 13:04:20 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 sudo apt-get install openjdk-17-jdk -y
 sudo apt-get install openjdk-11-jdk -y
-
+sudo apt install temurin-17-jdk -y
+/usr/bin/java --version
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
+
+sudo apt-get update -y
 sudo apt-get install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+
+
+
